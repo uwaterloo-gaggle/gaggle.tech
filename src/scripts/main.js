6c12abbc89b2eacd79eb9fa14ce3fc46f0288695
Atif Mahmud <atifmahmud101@gmail.com>
2019-01-30 02:10:23 -0500

    Update homepage contents

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,10 +10,9 @@ function type( strings ) {
     typeSpeed: 100,
     backSpeed: 20,
     loop: true,
-    loopCount: Infinity
+    loopCount: 1
   });
 }
-
 
 function navActivePage(){
   $('nav li a[href=".' + location.pathname + '"]').addClass('active');
@@ -25,6 +24,18 @@ function movingBackgroundImage(){
   var bg = document.querySelector('.hero-full-container');
   var windowWidth = window.innerWidth / 5;
   var windowHeight = window.innerHeight / 5 ;
+  var images = [
+    "silk1.jpg",
+    "silk2.jpg",
+    "silk3.jpg",
+    "silk4.jpg",
+    "silk5.jpg",
+    "silk6.jpg",
+    "silk7.jpg",
+    "silk8.jpg"
+  ];
+
+  bg.style.backgroundImage = 'url(./assets/images/' + images[Math.floor(Math.random() * images.length)] + ')';
 
   bg.addEventListener('mousemove', (e) => {
     var mouseX = e.clientX / windowWidth;
